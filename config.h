560e238cbf765fd58226a62b62abca0f35d34cfa
whatust <whatust@gmail.com>
2020-06-08 16:49:29 -0300

    Changed background from window name

--- config.h
+++ config.h
@@ -11,8 +11,8 @@ static const int swallowfloating    = 0;   /* 1 means swallow floating windows b
 static const int smartgaps          = 0;   /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;   /* 0 means no bar */
 static const int topbar             = 1;   /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
-static char dmenufont[]             = "Source Code Pro:size=10";
+static const char *fonts[]          = { "mono:size=10", "FontAwesome:pixelsize=14:antialias=true:autohint=true" };
+static char dmenufont[]             = "mono:size=10";
 static char normbgcolor[]           = "#1b2b34";
 static char normbordercolor[]       = "#555555";
 static char normfgcolor[]           = "#a7adba";
